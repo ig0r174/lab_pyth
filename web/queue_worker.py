f8245b974ffff26bae4647a85e18a961970180e1
1 <hstllbg@gmail.com>
2022-09-29 12:22:48 +0500

    RabbitMQ Send fix

--- web/queue_worker.py
+++ web/queue_worker.py
@@ -4,13 +4,13 @@ import os
 import models
 import pika
 
+connection = pika.BlockingConnection(pika.URLParameters(os.environ['RABBITMQ_URL']))
+channel = connection.channel()
+
 
 def send_message_to_queue(link: models.Link) -> None:
-    connection = pika.BlockingConnection(pika.URLParameters(os.environ['RABBITMQ_URL']))
-    channel = connection.channel()
     channel.queue_declare(queue=os.environ['QUEUE_NAME'])
     channel.basic_publish(exchange='', routing_key=os.environ['QUEUE_NAME'], body=json.dumps({
         'id': link.id,
         'url': link.url
     }).encode('utf-8'))
-    connection.close()
